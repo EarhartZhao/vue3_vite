524d78dfc75da3ca46dd336886f52068e80057b1
Earhart_Zhao <592752745@qq.com>
2021-05-11 18:24:17 +0800

    添加settime页面

---
+++ packages/vue3_elementuiPlus/src/views/main/expand/testTimeout.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class=""></div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive } from "vue";
+export default defineComponent({
+  name: "timeout",
+  setup() {
+    const data = reactive({});
+
+    return {
+      data,
+    };
+  },
+});
+</script>
