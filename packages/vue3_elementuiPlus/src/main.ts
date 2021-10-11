5a52778185cefa97d79be2c2b36d65bae1515872
Earhart_Zhao <592752745@qq.com>
2021-10-11 13:46:04 +0800

    升级依赖

--- packages/vue3_elementuiPlus/src/main.ts
+++ packages/vue3_elementuiPlus/src/main.ts
@@ -1,10 +1,11 @@
-import 'element-plus/lib/theme-chalk/index.css'
+import 'element-plus/dist/index.css'
 import './assets/styles/export/index.scss' // 全局scss
 import './assets/styles/default/index.scss' // 默认scss
 import './router/permission' //路由守卫
 import './components/index' //全局组件
 import 'dayjs/locale/zh-cn'
 
+// import { ElButton } from 'element-plus'
 import ElementPlus from 'element-plus'
 import locale from 'element-plus/lib/locale/lang/zh-cn'
 import Particles from 'particles.vue3'
@@ -12,3 +13,6 @@ import Particles from 'particles.vue3'
 import { app } from './app'
 
 app.use(Particles).use(ElementPlus, { size: "small", locale }).mount("#app");
+// app.use(Particles).use(ElementPlus, { size: "small" }).mount("#app");
+// app.config.globalProperties.$ELEMENT = { size: 'small' }
+// app.use(Particles).use(ElButton).mount("#app");
